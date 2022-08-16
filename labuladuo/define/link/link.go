3899bf79ff43df2997765dc98378ea3bbca03021
wangjing <wangjing@aggrx.com>
2022-08-16 19:00:01 +0800

    search node from link

--- labuladuo/define/link/link.go
+++ labuladuo/define/link/link.go
@@ -9,6 +9,10 @@ type Node struct {
 
 func EchoLink(link *Node) {
 	fmt.Println("链表的输出结果为：")
+	if link == nil {
+		fmt.Printf("链表内容为空")
+	}
+
 	for link != nil {
 		fmt.Printf("%d\t", link.Val)
 		link = link.Next

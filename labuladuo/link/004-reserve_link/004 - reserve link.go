bdfeb09a7edee7ab4a825c87e58a55321bd8895b
wangjing <wangjing@aggrx.com>
2022-08-17 17:03:53 +0800

    reserve link

--- labuladuo/link/004-reserve_link/004 - reserve link.go
+++ labuladuo/link/004-reserve_link/004 - reserve link.go
@@ -58,19 +58,68 @@ func reserveLinkFromDescK(k int, head *link.Node) *link.Node {
 	return newLink
 }
 
-// 3. k个一组转置链表
+// 3. 根据区间反转链表 - 区间为左闭右开
+func reserveLinkByRangeAB(a, b, head *link.Node) *link.Node {
+	// search a
+	newLink := head
+	for head.Next != a {
+		head = head.Next
+	}
+
+	var prev, cur, next *link.Node
+	prev, cur, next = b, a, a
+
+	for cur != b {
+		next = cur.Next
+		cur.Next = prev
+
+		prev = cur
+		cur = next
+	}
+
+	newLink.Next = prev
+
+	return newLink
+}
+
+//已a为头结点范围反转
+func reserve(a, b *link.Node) *link.Node {
+	var prev, cur, next *link.Node
+	prev, cur, next = nil, a, a
+
+	for cur != b {
+		next = cur.Next
+		cur.Next = prev
+
+		prev = cur
+		cur = next
+	}
+
+	return prev
+}
+
+// 4. k个一组转置链表 - 递归 + 迭代
 func reserveLinkGroupByK(k int, head *link.Node) *link.Node {
-	if k == 0 || head == nil || head.Next == nil {
+	if k <= 1 || head == nil || head.Next == nil {
 		return head
 	}
 
-	return nil
+	a, b := head, head
+	count := 0
+	for b != nil && count < k {
+		count++
+		b = b.Next
+	}
+	newLink := reserve(a, b)
+	a.Next = reserveLinkGroupByK(k, b)
+
+	return newLink
 }
 
 func main() {
 	// 1. 链表转置
 	head1 := &link.Node{Val: 1, Next: &link.Node{Val: 3, Next: &link.Node{Val: 5, Next: &link.Node{Val: 7, Next: &link.Node{Val: 9}}}}}
-	fmt.Println("1. 链表转置")
+	fmt.Println("1. 链表转置：")
 	link.EchoLink(head1, "转置前链表为：")
 	reservedHead := reserveLink(head1)
 	link.EchoLink(reservedHead, "转置后链表为：")
@@ -86,10 +135,24 @@ func main() {
 
 	line.SplitLine()
 
-	// 3，k个一组进行链表转置
-	head3 := &link.Node{Val: 1, Next: &link.Node{Val: 3, Next: &link.Node{Val: 5, Next: &link.Node{Val: 7, Next: &link.Node{Val: 9, Next: &link.Node{Val: 11}}}}}}
-	fmt.Println(head3)
-	fmt.Println("3. k个一组进行链表转置：")
-	link.EchoLink(head3, "转置前链表为：")
-	link.EchoLink(head3, "转置后链表为：")
+	// 3. 根据特定区间反转链表
+	node11 := &link.Node{Val: 11}
+	node9 := &link.Node{Val: 9, Next: node11}
+	node7 := &link.Node{Val: 7, Next: node9}
+	node5 := &link.Node{Val: 5, Next: node7}
+	node3 := &link.Node{Val: 3, Next: node5}
+	node1 := &link.Node{Val: 1, Next: node3}
+	fmt.Println("3. 根据区间反转链表：")
+	link.EchoLink(node1, "反转前的链表为：")
+	linkByRangeAB := reserveLinkByRangeAB(node3, node9, node1)
+	link.EchoLink(linkByRangeAB, "反转之后的链表为：")
+
+	line.SplitLine()
+
+	// 4，k个一组进行链表转置
+	head4 := &link.Node{Val: 1, Next: &link.Node{Val: 3, Next: &link.Node{Val: 5, Next: &link.Node{Val: 7, Next: &link.Node{Val: 9, Next: &link.Node{Val: 11}}}}}}
+	fmt.Println("4. k个一组进行链表转置：")
+	link.EchoLink(head4, "转置前链表为：")
+	linkGroupByK := reserveLinkGroupByK(2, head4)
+	link.EchoLink(linkGroupByK, "转置后链表为：")
 }

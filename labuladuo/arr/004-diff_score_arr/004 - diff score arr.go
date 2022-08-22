5db4bb57cea42a04d12e62f06a213a9296c88a93
wangjing <wangjing@aggrx.com>
2022-08-22 11:24:10 +0800

    diff sum arr - (差分数组执行批量增减)

--- labuladuo/arr/004-diff_score_arr/004 - diff score arr.go
+++ labuladuo/arr/004-diff_score_arr/004 - diff score arr.go
@@ -75,9 +75,24 @@ func flightReservation(n int, updates [][]int) []int {
 }
 
 // 3. 拼车 - 输入乘客表， [num, start, end] 代表 num 个旅客要从 start 上车， end 下车， 输出：一次能否把所有乘客拉完， 且不能超载， 客车容量为 capacity
-func carPool(arr [][]int) bool {
+func carPool(n, capacity int, updates [][]int) bool {
+	nums := make([]int, n)
+	difference := &Difference{Diff: nums}
 
-	return false
+	difference.Diff[0] = updates[0][0]
+	for i := 0; i < len(updates); i++ {
+		start, end, val := updates[i][1], updates[i][2]-1, updates[i][0]
+		difference.increment(start, end, val)
+	}
+
+	reduction := difference.reduction()
+	for _, v := range reduction {
+		if v > capacity {
+			return false
+		}
+	}
+
+	return true
 }
 
 func main() {
@@ -96,4 +111,12 @@ func main() {
 	fmt.Println("原数组的长度和操作三元组为：", length1, updates1)
 	reservation := flightReservation(length1, updates1)
 	fmt.Println("航班预定的统计结果为结果为：", reservation)
+
+	line.SplitLine()
+
+	update2 := [][]int{{2, 1, 5}, {3, 3, 7}}
+	capacity := 4
+	fmt.Println("车的最大容量和操作三元组为：", capacity, update2)
+	res := carPool(1001, capacity, update2)
+	fmt.Println("拼车的结果为：", res)
 }

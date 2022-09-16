c04d78e9348def98de08c653b3693c3de0c6a4d8
wangjing <wangjing@aggrx.com>
2022-09-16 16:29:28 +0800

    dp2 - init

---
+++ labuladuo/dp/002-subtype_question/002 - subtype question.go
@@ -0,0 +1,47 @@
+package main
+
+// 编辑距离
+// 给你两个单词word1 和word2， 请返回将word1转换成word2 所使用的最少操作数
+// 你可以对一个单词进行如下三种操作：插入、删除、替换
+func minDistance(word1 string, word2 string) int {
+
+	return 0
+}
+
+// 最大子数组和
+// 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。子数组 是数组中的一个连续部分
+func maxSubArray(nums []int) int {
+
+	return 0
+}
+
+// 最长公共子序列
+func longestCommonSubsequence(text1 string, text2 string) int {
+
+	return 0
+}
+
+// 两个字符串的删除操作
+// 给定两个单词 word1 和 word2 ，返回使得 word1 和  word2 相同所需的最小步数，仅删除操作
+func minDistance1(word1 string, word2 string) int {
+
+	return 0
+}
+
+// 两个字符串的最小ASCII删除和
+// 给定两个字符串s1 和 s2，返回 使两个字符串相等所需删除字符的 ASCII 值的最小和
+func minimumDeleteSum(s1 string, s2 string) int {
+
+	return 0
+}
+
+// 让字符串成为回文串的最少插入次数
+// 给你一个字符串s，每一次操作你都可以在字符串的任意位置插入任意字符。返回让s成为回文串的最少操作次数
+func minInsertions(s string) int {
+
+	return 0
+}
+
+func main() {
+
+}
